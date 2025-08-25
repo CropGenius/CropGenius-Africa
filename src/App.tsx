782d3ee5bddbf05a9386068e67ec7eccb4f6fc16
CropGenius <brianmuthurikimathi@gmail.com>
2025-08-25 15:15:21 +0300

    till when

--- src/App.tsx
+++ src/App.tsx
@@ -44,23 +44,40 @@ export default function App() {
           <Routes>
             <Route path="/auth" element={<Auth />} />
             <Route path="/auth/callback" element={<OAuthCallback />} />
+            <Route path="/join" element={<JoinPage />} />
+            
+            <Route path="/onboarding" element={<OnboardingPage />} />
+            
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route
-              path="/dashboard"
-              element={
-                <Protected>
-                  <Index />
-                </Protected>
-              }
-            />
-            <Route
-              path="/onboarding"
-              element={
-                <Protected>
-                  <OnboardingPage />
-                </Protected>
-              }
-            />
+            <Route path="/dashboard" element={<Protected><Index /></Protected>} />
+            <Route path="/fields" element={<Protected><Fields /></Protected>} />
+            <Route path="/fields/:id" element={<Protected><FieldDetail /></Protected>} />
+            <Route path="/fields/:id/crops/add" element={<Protected><AddCrop /></Protected>} />
+            <Route path="/fields/:id/activity" element={<Protected><FieldDetail /></Protected>} />
+            <Route path="/weather" element={<Protected><Weather /></Protected>} />
+            <Route path="/scan" element={<Protected><Scan /></Protected>} />
+            <Route path="/chat" element={<Protected><Chat /></Protected>} />
+            
+            <Route path="/settings" element={<Protected><Settings /></Protected>} />
+            <Route path="/tasks" element={<Protected><Tasks /></Protected>} />
+            <Route path="/more" element={<Protected><More /></Protected>} />
+            <Route path="/manage-fields" element={<Protected><ManageFields /></Protected>} />
+            <Route path="/alerts" element={<Protected><Alerts /></Protected>} />
+            <Route path="/task-manager" element={<Protected><TaskManager /></Protected>} />
+            <Route path="/community" element={<Protected><QuestionsPage /></Protected>} />
+            <Route path="/community/questions/:questionId" element={<Protected><QuestionDetailPage /></Protected>} />
+            <Route path="/community/legacy" element={<Protected><Community /></Protected>} />
+            <Route path="/farm-planning" element={<Protected><FarmPlanningPage /></Protected>} />
+            <Route path="/crop-disease-detection" element={<Protected><CropDiseaseDetectionPage /></Protected>} />
+            <Route path="/yield-predictor" element={<Protected><YieldPredictor /></Protected>} />
+            <Route path="/market-insights" element={<Protected><MarketInsightsPage /></Protected>} />
+            <Route path="/mission-control" element={<Protected><MissionControlPage /></Protected>} />
+            <Route path="/upgrade" element={<Protected><UpgradePage /></Protected>} />
+            <Route path="/payment-callback" element={<PaymentCallback />} />
+            <Route path="/referrals" element={<Protected><ReferralsPage /></Protected>} />
+            <Route path="/subscription-settings" element={<Protected><SubscriptionSettings /></Protected>} />
+            
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Toaster />
         </Router>
